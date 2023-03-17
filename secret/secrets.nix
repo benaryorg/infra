ef79d668a3fbd83b35af3af96f1d17574028a6ac
benaryorg <binary@benary.org>
2023-03-17 13:42:43 +0000

    lxd2.cloud.bsocat.net
    Signed-off-by: benaryorg <binary@benary.org>

--- secret/secrets.nix
+++ secret/secrets.nix
@@ -8,6 +8,7 @@ in
   {
     "lego/hedns/lxd6.cloud.bsocat.net.age".publicKeys = [ conf.hostkey."lxd6.cloud.bsocat.net" ];
     "lego/hedns/lxd4.cloud.bsocat.net.age".publicKeys = [ conf.hostkey."lxd4.cloud.bsocat.net" ];
+    "lego/hedns/lxd2.cloud.bsocat.net.age".publicKeys = [ conf.hostkey."lxd2.cloud.bsocat.net" ];
     "lego/hedns/lxd1.cloud.bsocat.net.age".publicKeys = [ conf.hostkey."lxd1.cloud.bsocat.net" ];
     "service/syncplay/syncplay.lxd.bsocat.net.age".publicKeys = [ conf.hostkey."syncplay.lxd.bsocat.net" ];
   }
