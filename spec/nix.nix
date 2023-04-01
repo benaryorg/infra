aa745507d66c943ab725a326b59084862475af1e
benaryorg <binary@benary.org>
2023-04-01 17:33:12 +0000

    nix: cleaner activation script
    Signed-off-by: benaryorg <binary@benary.org>

--- spec/nix.nix
+++ spec/nix.nix
@@ -8,7 +8,7 @@ with lib;
     systemd.services.nix-daemon.environment.TMPDIR = "/nix/tmp";
     system.activationScripts.nix-tmpdir =
     ''
-      btrfs subvolume create /nix/tmp || mkdir -p /nix/tmp
+      test -d /nix/tmp || ${pkgs.btrfs-progs}/bin/btrfs subvolume create /nix/tmp || mkdir -p /nix/tmp
     '';
   };
 }
