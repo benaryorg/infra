62bfd614a8622c6f6aab34667300017a43756e2f
benaryorg <binary@benary.org>
2023-10-22 12:26:46 +0000

    git: cgit frontend
    Signed-off-by: benaryorg <binary@benary.org>

--- spec/git.nix
+++ spec/git.nix
@@ -81,67 +81,31 @@ with lib;
           {
             forceSSL = true;
             enableACME = true;
-            locations."/" =
-            {
-              proxyPass = "http://localhost:8000";
-            };
-            locations."~ \"^/[a-zA-Z0-9._-]+/(git-(receive|upload)-pack|HEAD|info/refs|objects/(info/(http-)?alternates|packs)|[0-9a-f]{2}/[0-9a-f]{38}|pack/pack-[0-9a-f]{40}\\.(pack|idx))$\"" =
-            {
-              fastcgiParams = 
-              {
-                SCRIPT_FILENAME = "${pkgs.git}/bin/git-http-backend";
-                GIT_PROJECT_ROOT = "/var/lib/gitolite/repositories/public";
-                GIT_HTTP_EXPORT_ALL = "";
-                PATH_INFO = "$fastcgi_script_name";
-              };
-              extraConfig =
-              ''
-                fastcgi_read_timeout 1800;
-                fastcgi_pass unix:/run/fcgiwrap.sock;
-              '';
-            };
           };
         };
       };
-      fcgiwrap =
+    };
+
+    services.cgit.${config.networking.fqdn} =
+    {
+      enable = true;
+      scanPath = "/var/lib/gitolite/repositories/public";
+      settings =
       {
-        enable = true;
-        group = "git";
-        # important; security vulns in git will have major impact here
-        # anonymous write here is only soft-disabled by upstream sanity checks against the repo config
-        # https://git-scm.com/docs/git-daemon#Documentation/git-daemon.txt-receive-pack
-        user = "git";
-        preforkProcesses = 16;
+        clone-url = "https://${config.networking.fqdn}/$CGIT_REPO_URL";
+        branch-sort = "age";
+        root-title = "${config.networking.fqdn} by Katze";
+        root-desc = "because self-hosted is better";
+        snapshots = "tar.gz";
+        readme = ":README.md";
+        enable-index-owner = false;
+        enable-follow-links = true;
+        remove-suffix = true;
+        source-filter = "${pkgs.cgit}/lib/cgit/filters/syntax-highlighting.py";
+        about-filter = "${pkgs.cgit}/lib/cgit/filters/about-formatting.sh";
       };
     };
-    systemd.services =
-      {
-        klaus =
-          let
-            klausGunicorn = pkgs.python3.withPackages (ps: with ps; [ gunicorn markdown klaus ]);
-          in
-            {
-              enable = true;
-              description = "klaus git viewer";
-              path = [ pkgs.git ];
-              environment =
-              {
-                KLAUS_SITE_NAME = "${config.networking.fqdn} by Katze";
-                KLAUS_REPOS_ROOT = "/var/lib/gitolite/repositories/public";
-                KLAUS_USE_SMARTHTTP = "false";
-              };
-              serviceConfig =
-              {
-                Type = "simple";
-                ExecStart = "${klausGunicorn}/bin/gunicorn --threads 8 --timeout 120 klaus.contrib.wsgi_autoreload";
-                User = "git";
-                Group = "git";
-              };
-              wantedBy = [ "multi-user.target" ];
-            };
-      }
-      //
-      lib.pipe config.benaryorg.git.mirror
+    systemd.services = lib.pipe config.benaryorg.git.mirror
       [
         builtins.attrValues
         (builtins.map (config:
