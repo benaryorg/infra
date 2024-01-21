e4e9e73a523207b0507794fdfeb4692ffd2e8239
benaryorg <binary@benary.org>
2024-01-21 00:13:06 +0000

    nixos-aarch64: aarch64 build server
    Signed-off-by: benaryorg <binary@benary.org>

--- flake.nix
+++ flake.nix
@@ -162,8 +162,36 @@
             role = "server";
             publicKey = "nixos.home.bsocat.net:yiK6zWXrGJRUw2LqhSqr9x1H6jbeLl/nokgJJBVJZ80=";
             privateKeyFile = config.age.secrets.buildSecret.path;
+            systems = [ "x86_64-linux" ];
           };
 
+          fileSystems."/nix/tmp" = { fsType = "tmpfs"; options = [ "noatime" "size=8g" ]; };
+
+          networking.nameservers = [ "2a0c:b641:a40:5::" ];
+
+          system.stateVersion = "23.11";
+        };
+
+        "nixos-aarch64.home.bsocat.net" = { name, nodes, pkgs, lib, config, modulesPath, ... }:
+        {
+          benaryorg.ssh.hostkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICST4L1PUhwaRr0Elq618UqWn/oNytDblkmEG3UDao3b";
+
+          age.secrets.buildSecret.file = ./secret/build/nixos-aarch64.home.bsocat.net.age;
+          benaryorg.net.type = "manual";
+          benaryorg.ssh.x11 = true;
+          benaryorg.user.ssh.keys = lib.mkAfter [ userkeys."benaryorg@gnutoo.home.bsocat.net" ];
+          benaryorg.prometheus.client.enable = true;
+
+          benaryorg.build =
+          {
+            role = "server";
+            publicKey = "nixos-aarch64.home.bsocat.net:pV77z/+Ovmn/fK5YtV6FANXG+q4x926/wFhxqet/rWU=";
+            privateKeyFile = config.age.secrets.buildSecret.path;
+            systems = [ "aarch64-linux" ];
+            tags = lib.mkAfter [ "aarch64-linux" ];
+          };
+
+          nix.settings.extra-platforms = [ "aarch64-linux" ];
           fileSystems."/nix/tmp" = { fsType = "tmpfs"; options = [ "noatime" "size=8g" ]; };
 
           networking.nameservers = [ "2a0c:b641:a40:5::" ];
@@ -1025,7 +1053,7 @@
           benaryorg.ssh.hostkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHcgG0ngQ0kAARuIIuh6V43uObAnFBZsGCgxFs/OvW62";
 
           benaryorg.prometheus.client.enable = true;
-          benaryorg.build.tags = [ "shell.bsocat.net" "aarch64" ];
+          benaryorg.build.tags = [ "shell.bsocat.net" "aarch64-linux" ];
 
           nix.settings.allowed-uris = [ "https://git.shell.bsocat.net/" ];
           services.nginx =
