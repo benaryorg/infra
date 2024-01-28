6aceec31b89c2c4c38c2178cecb1a93317da6e4e
benaryorg <binary@benary.org>
2024-01-28 16:00:22 +0000

    nix: allow fallback
    Hey, look, having part of your infra go down shows you issues in your code, who woulda thunk -.-
    
    Signed-off-by: benaryorg <binary@benary.org>

--- spec/nix.nix
+++ spec/nix.nix
@@ -7,6 +7,7 @@
       experimental-features = [ "nix-command" "flakes" ];
       keep-outputs = true;
       builders-use-substitutes = true;
+      fallback = true;
     };
     nix.gc.automatic = true;
     nix.gc.options = "--delete-older-than 16d";
