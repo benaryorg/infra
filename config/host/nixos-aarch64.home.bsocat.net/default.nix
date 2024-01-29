88895870dbf941f346471c32e2c82d28e57e9c5b
benaryorg <binary@benary.org>
2024-01-29 18:42:59 +0000

    nixos-aarch64: binfmt emulation
    Signed-off-by: benaryorg <binary@benary.org>

--- config/host/nixos-aarch64.home.bsocat.net/default.nix
+++ config/host/nixos-aarch64.home.bsocat.net/default.nix
@@ -17,7 +17,9 @@
     tags = lib.mkAfter [ "aarch64-linux" ];
   };
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   nix.settings.extra-platforms = [ "aarch64-linux" ];
+
   fileSystems."/nix/tmp" = { fsType = "tmpfs"; options = [ "noatime" "size=8g" ]; };
 
   networking.nameservers = [ "2a0c:b641:a40:5::" ];
