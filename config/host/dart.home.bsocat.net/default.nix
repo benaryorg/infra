ddc73413c68f0b6dc1d7b2458885d0db6801b59f
benaryorg <binary@benary.org>
2024-01-29 18:43:49 +0000

    dart: factory reset
    Signed-off-by: benaryorg <binary@benary.org>

--- config/host/dart.home.bsocat.net/default.nix
+++ config/host/dart.home.bsocat.net/default.nix
@@ -1,7 +1,7 @@
 { name, nodes, pkgs, lib, config, ... }:
 {
-  benaryorg.ssh.hostkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPOwEKWHIPHTxmG2IbB/D0S6AJejI3FfQvZJqOSmnKGi";
-  benaryorg.ssh.userkey.benaryorg = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPp1WESxGNUYOwx3J9W+s0Z7ZqwKx3Jp825tKxE3I8EG";
+  benaryorg.ssh.hostkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBz6svwHHhGz+/C2FKHCC1XaKEnaHc20fp+YOQTojpzU";
+  benaryorg.ssh.userkey.benaryorg = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIpkNc/B083GREqQrk6sOciWvC4k9uU6J3K33LErICQy";
 
   nixpkgs.system = "aarch64-linux";
 
