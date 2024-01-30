63ca0c76fe11a67efbc5e6d22b4b1c3653c29da6
benaryorg <binary@benary.org>
2024-01-30 07:19:14 +0000

    gnutoo: ipfs pin sync
    Signed-off-by: benaryorg <binary@benary.org>

--- config/host/gnutoo.home.bsocat.net/ipfs.nix
+++ config/host/gnutoo.home.bsocat.net/ipfs.nix
@@ -18,4 +18,37 @@
   # specifically disable the config merging for now
   # this shall be improved upon in the future
   systemd.services.ipfs.preStart = lib.mkForce "";
+
+  systemd.services.ipfs-pin-sync =
+  {
+    description = "sync IPFS pins stored in MFS";
+    after = [ "network-online.target" "ipfs-api.socket" ];
+    wants = [ "ipfs-api.socket" ];
+    serviceConfig =
+    {
+      Type = "oneshot";
+      User = config.services.kubo.user;
+      Group = config.services.kubo.group;
+    };
+    environment.IPFS_PATH = config.services.kubo.dataDir;
+    path = with pkgs; [ kubo util-linux moreutils gnugrep util-linux zsh ];
+    script =
+    ''
+      set -e
+      test -e $IPFS_PATH/repo.lock
+      ipfs cat /ipns/12D3KooWNoPhenCQSsdfKJvJ8g2R1bHbw7M7s5arykhqJCVd5F2B/meta/update-pins | ifne flock -e -w 4 $IPFS_PATH zsh
+    '';
+  };
+  systemd.timers.ipfs-pin-sync =
+  {
+    description = "sync IPFS pins stored in MFS";
+    after = [ "network-online.target" "ipfs-api.socket" ];
+    wantedBy = [ "timers.target" ];
+    timerConfig =
+    {
+      OnUnitInactiveSec = 60;
+      OnBootSec = 600;
+      Persistent = true;
+    };
+  };
 }
