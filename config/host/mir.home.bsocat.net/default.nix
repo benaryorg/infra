527fd3dc1b91e0d7621015ff16f32c891093a3c2
benaryorg <binary@benary.org>
2024-01-30 18:37:30 +0000

    mir: ceph package contains ceph-volume tooling
    Signed-off-by: benaryorg <binary@benary.org>

--- config/host/mir.home.bsocat.net/default.nix
+++ config/host/mir.home.bsocat.net/default.nix
@@ -40,7 +40,7 @@
     xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     xdg.portal.config.common.default = "gtk";
     services.flatpak.enable = true;
-    users.users.benaryorg.packages = with pkgs; [ gnupg vim-full ceph-client kubo ];
+    users.users.benaryorg.packages = with pkgs; [ gnupg vim-full ceph kubo ];
 
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true;
