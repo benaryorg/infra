619ff6fd4c09eb749c0a91ff45f86709f3bbf8f7
benaryorg <binary@benary.org>
2024-02-13 23:46:15 +0000

    gnutoo: no backlight control
    Signed-off-by: benaryorg <binary@benary.org>

--- config/host/gnutoo.home.bsocat.net/desktop.nix
+++ config/host/gnutoo.home.bsocat.net/desktop.nix
@@ -5,6 +5,7 @@
   benaryorg.home-manager.perUserSettings.benaryorg.benaryorg.desktop.extraInitCommands = [ "xrandr --output DP-3 --primary --mode 2560x1440 --rate 144 --pos 0x0 --output DP-1 --dpi 96 --off --output DP-0 --off || true" ];
 
   services.logind.lidSwitch = "ignore";
+  systemd.suppressedSystemUnits = lib.mkAfter [ "systemd-backlight@.service" ];
   nixpkgs.config.allowUnfree = true;
   hardware.nvidia.open = false;
   services.xserver.videoDrivers = [ "nvidia" ];
