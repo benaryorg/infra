8279fea18361c96410571ac0c4287b98c2bfd67a
benaryorg <binary@benary.org>
2024-03-07 04:31:10 +0000

    gnutoo: systemd-boot
    Signed-off-by: benaryorg <binary@benary.org>

--- config/host/gnutoo.home.bsocat.net/boot.nix
+++ config/host/gnutoo.home.bsocat.net/boot.nix
@@ -3,52 +3,8 @@
   boot.initrd.supportedFilesystems = [ "ext2" "ext4" "vfat" "btrfs" ];
   boot.supportedFilesystems = [ "ext2" "ext4" "vfat" "btrfs" ];
   boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = false;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
-  # efibootmgr -c -b 3 --disk /dev/sda --label nixos --loader '\default\kernel' -u 'initrd=\default\initrd loglevel=4 verbose'
-  boot.loader.external =
-  {
-    enable = true;
-    installHook = pkgs.writeShellScript "copy-kernel"
-    ''
-      #! ${pkgs.zsh}
-      set -e
-
-      system=''${1?"no system provided"}
-      if ! test -e $system; then
-        printf "system %q does not exist\\n" $system
-        false
-      fi
-
-      jq=${pkgs.jq}/bin/jq
-
-      kernel=$($jq -r '.["org.nixos.bootspec.v1"].kernel' $system/boot.json)
-      initrd=$($jq -r '.["org.nixos.bootspec.v1"].initrd' $system/boot.json)
-      init=$($jq -r '.["org.nixos.bootspec.v1"].init' $system/boot.json)
-
-      ${pkgs.coreutils}/bin/mkdir -p /boot/default
-      ${pkgs.coreutils}/bin/install --group=root --owner=root --mode=644 /boot/default/kernel /boot/default/kernel.old || true
-      ${pkgs.coreutils}/bin/install --group=root --owner=root --mode=644 /boot/default/initrd /boot/default/initrd.old || true
-      ${pkgs.coreutils}/bin/install --group=root --owner=root --mode=644 $kernel /boot/default/kernel
-      ${pkgs.coreutils}/bin/install --group=root --owner=root --mode=644 $initrd /boot/default/initrd
-      ${pkgs.coreutils}/bin/mkdir -p /sbin
-      ${pkgs.coreutils}/bin/ln -sf $system/init /sbin/init
-
-      true
-    '';
-  };
-  boot.initrd.systemd.services.initrd-nixos-activation.script = lib.mkForce
-  ''
-    set -euo pipefail
-    export PATH="/bin:${config.boot.initrd.systemd.package.util-linux}/bin"
-    init="$(readlink /sysroot/sbin/init)"
-    closure="$(dirname "$init")"
-    echo 'NEW_INIT=' > /etc/switch-root.conf
-    mkdir -p /sysroot/run
-    mount --bind /run /sysroot/run
-    export IN_NIXOS_SYSTEMD_STAGE1=true
-    exec chroot /sysroot "$closure/prepare-root"
-  '';
 
   boot.initrd.systemd.enable = true;
   boot.initrd.luks.devices =
