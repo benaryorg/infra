e58e74d48b7ab24a05c62eb3d709d268080b9678
benaryorg <binary@benary.org>
2024-06-18 16:35:37 +0000

    nix: use lix
    I'll just copy verbatim what I sent a friend below:
    
    Okay, I can reproducibly run:
    
    ```bash
    nix build --no-link --print-build-logs --print-out-paths tarball+https://git.shell.bsocat.net/infra/snapshot/infra-update.tar.gz#nixosConfigurations.nixos-builder.config.age.secrets.buildSecret.file -v --refresh --override-input nixpkgs https://git.shell.bsocat.net/nixpkgs/snapshot/nixpkgs-nixos-24.05.tar.gz
    ```
    
    And it works the first time, but the second time I run it, it goes:
    
    ```text
    warning: error: unable to download 'https://git.shell.bsocat.net/infra/snapshot/infra-update.tar.gz': HTTP error 200 (curl error: Failure when receiving data from the peer); retrying in 353 ms
    warning: error: unable to download 'https://git.shell.bsocat.net/infra/snapshot/infra-update.tar.gz': HTTP error 200 (curl error: Failure when receiving data from the peer); retrying in 646 ms
    warning: error: unable to download 'https://git.shell.bsocat.net/infra/snapshot/infra-update.tar.gz': HTTP error 200 (curl error: Failure when receiving data from the peer); retrying in 1409 ms
    warning: error: download of 'https://git.shell.bsocat.net/infra/snapshot/infra-update.tar.gz' was interrupted; using cached version
    ```
    
    However the best part about all this is that the issue does *not* happen with the lebiams Nix fork that I wanted to use anyway (Lix).
    It just fucking works there.
    Deleting *~/.cache/nix* makes it work once again for one run.
    
    Signed-off-by: benaryorg <binary@benary.org>

--- spec/nix.nix
+++ spec/nix.nix
@@ -11,6 +11,8 @@
       # apparently nix requires this now to build properly in restricted mode, and it does so with all three schemas
       allowed-uris = lib.mkOrder 1000 (builtins.map (schema: "${schema}://git.shell.bsocat.net/") [ "https" "git+https" "tarball+https" ]);
     };
+    # use the lebiams Nix
+    nix.package = pkgs.lix;
     nix.gc.automatic = true;
     nix.gc.options = "--delete-older-than 16d";
     nixpkgs.flake.source = pkgs.path;
